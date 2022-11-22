1f12c0f7a0da2090676f5bbc074f8a4bd03fb8ac
fhchl <fhchl@users.noreply.github.com>
2022-11-22 16:23:07 +0100

    fitting with bounds works ONLY for pytrees without ndarray leaves

--- src/dynax/estimation.py
+++ src/dynax/estimation.py
@@ -5,12 +5,13 @@ import equinox as eqx
 import jax
 import jax.numpy as jnp
 import numpy as np
-from jax.tree_util import tree_map
+from jax.tree_util import tree_flatten, tree_map, tree_structure
 from scipy.optimize import least_squares
 from scipy.optimize._optimize import MemoizeJac
 
 from .custom_types import Array, PyTree
-from .system import DiscreteForwardModel, ForwardModel, spline_it
+from .system import (DiscreteForwardModel, DynamicalSystem, ForwardModel,
+                     spline_it)
 from .util import value_and_jacfwd
 
 
@@ -25,18 +26,25 @@ def boxed_field(lower: float, upper: float, **kwargs):
   metadata["constrained"] = ("boxed", (lower, upper))
   return field(**kwargs)
 
+
 def non_negative_field(min_val=0., **kwargs):
   """Mark a dataclass field as having a non-negative value."""
   return boxed_field(lower=min_val, upper=np.inf, **kwargs)
 
-def build_bounds(self: eqx.Module) -> Tuple[PyTree]:
+
+# TODO: In the best case, this function should return PyTrees of the same form
+#       as `self`. Right now however, the function is NOT recursive, so won't
+#       work on e.g. ForwardModel.
+def build_bounds(self: DynamicalSystem) -> Tuple[PyTree, PyTree]:
   """Build PyTrees of lower and upper bounds."""
   lower_bounds = []
   upper_bounds = []
   for field_ in fields(self):
     name = field_.name
-    value = self.__dict__[name]
-    print(name, value)
+    try:
+      value = self.__dict__[name]
+    except KeyError:
+      continue
     # static parameters have no bounds
     if field_.metadata.get("static", False):
       continue
@@ -53,7 +61,10 @@ def build_bounds(self: eqx.Module) -> Tuple[PyTree]:
     else:
       lower_bounds.append(tree_map(lambda _: -np.inf, value))
       upper_bounds.append(tree_map(lambda _: np.inf, value))
-  return tuple(lower_bounds), tuple(upper_bounds)
+  treedef = tree_structure(self)
+  return (treedef.unflatten(tuple(lower_bounds)),
+          treedef.unflatten(tuple(upper_bounds)))
+
 
 def fit_ml(model: ForwardModel | DiscreteForwardModel,
            t: Array,
@@ -67,8 +78,13 @@ def fit_ml(model: ForwardModel | DiscreteForwardModel,
   if (isinstance(model, ForwardModel) and u is not None and
       not isinstance(u, Callable)):
     u = spline_it(t, u)
-  init_params, treedef = jax.tree_util.tree_flatten(model)
+
+  # TODO: if model or any sub tree has some ndarray leaves, this does not
+  # completely flatten the arrays. But a flat array is needed for `least_squares`.
+  # Same problem appears for the bounds.
+  init_params, treedef = tree_flatten(model)
   std_y = np.std(y, axis=0)
+  bounds = tuple(map(lambda x: tree_flatten(x)[0], build_bounds(model.system)))
 
   def residuals(params):
     model = treedef.unflatten(params)
@@ -83,6 +99,7 @@ def fit_ml(model: ForwardModel | DiscreteForwardModel,
   # - https://lmfit.github.io/lmfit-py/index.html
   # - https://github.com/dipolar-quantum-gases/jaxfit
   # - scipy.optimize.curve_fit
-  res = least_squares(fun, init_params, jac=jac, x_scale='jac', verbose=2)
+  res = least_squares(fun, init_params, bounds=bounds, jac=jac, x_scale='jac',
+                      verbose=2)
   params = res.x
   return treedef.unflatten(params)
