080e8f0c76b64bc6d37a07c0ef9c062ebe599e2e
fhchl <fhchl@users.noreply.github.com>
2022-12-08 15:04:46 +0100

    include examples in tests

---
+++ tests/test_examples.py
@@ -0,0 +1,11 @@
+import pathlib
+import runpy
+import pytest
+
+examples = (pathlib.Path(__file__, '..', '..', 'examples')
+            .resolve()
+            .glob('*.py'))
+
+@pytest.mark.parametrize('examples', examples)
+def test_examples_run_without_error(examples):
+    runpy.run_path(examples)
