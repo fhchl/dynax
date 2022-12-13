97eda623b13592102ef0b64cc42f7085cfe74d6b
fhchl <fhchl@users.noreply.github.com>
2022-12-13 14:10:57 +0100

    don't test examples by default

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -6,6 +6,7 @@ examples = (pathlib.Path(__file__, '..', '..', 'examples')
             .resolve()
             .glob('*.py'))
 
+@pytest.mark.slow
 @pytest.mark.parametrize('examples', examples)
 def test_examples_run_without_error(examples):
     runpy.run_path(examples)
