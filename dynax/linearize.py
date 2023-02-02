aa4a929e5ccd2e31181904f8b2c0db87613cf309
fhchl <fhchl@users.noreply.github.com>
2023-02-02 11:55:19 +0100

    feat: allow specifing output when linearizing

--- dynax/linearize.py
+++ dynax/linearize.py
@@ -1,6 +1,7 @@
 """Functions related to feedback linearization of nonlinear systems."""
 
 from collections.abc import Callable
+from typing import Optional
 
 import jax
 import numpy as np
@@ -10,13 +11,21 @@ from .derivative import lie_derivative
 from .system import ControlAffine, LinearSystem
 
 
-def relative_degree(sys, xs, max_reldeg=10) -> int:
+def relative_degree(sys, xs, max_reldeg=10, output: Optional[int] = None) -> int:
     """Estimate relative degree of system on region xs."""
     # TODO: when ControlAffine has y = h(x) + i(x)u, include test for n = 0,
     # i.e. i(x) == 0 for all x in xs.
-    assert sys.n_inputs == 1 and sys.n_outputs == 1, "only SISO supported"
+    assert sys.n_inputs == 1
+    if output is None:
+        assert (
+            sys.n_outputs == 1
+        ), f"Output is None, but system has {sys.n_outputs} outputs."
+        h = sys.h
+    else:
+        h = lambda *args, **kwargs: sys.h(*args, **kwargs)[output]
+
     for n in range(1, max_reldeg + 1):
-        LgLfn1h = lie_derivative(sys.g, lie_derivative(sys.f, sys.h, n - 1))
+        LgLfn1h = lie_derivative(sys.g, lie_derivative(sys.f, h, n - 1))
         res = jax.vmap(LgLfn1h)(xs)
         if np.all(res == 0.0):
             continue
@@ -35,9 +44,15 @@ def is_controllable(A, B) -> bool:
 
 
 def input_output_linearize(
-    sys: ControlAffine, reldeg: int, ref: LinearSystem
+    sys: ControlAffine, reldeg: int, ref: LinearSystem, output: Optional[int] = None
 ) -> Callable[[Array, Array, float], float]:
     """Construct input-output linearizing feedback law.
+
+    Args:
+        sys: nonlinear model with single input
+        reldeg: relative degree of `sys` and `ref`
+        ref: target model with single input
+        output: specify linearizing output if systems have multiple outputs
 
     Note:
         Relative degree of `ref` must be same or higher than degree of `sys`.
@@ -45,12 +60,18 @@ def input_output_linearize(
 
     """
     # TODO: add options for reference `normal_form` or zeros of polynomials
-    assert sys.n_inputs == 1 and sys.n_outputs == 1, "sys must be SISO"
-    assert ref.n_inputs == ref.n_outputs == 1, "ref must be SISO"
+    assert sys.n_inputs == ref.n_inputs == 1, "systems must be single input"
 
-    Lfnh = lie_derivative(sys.f, sys.h, reldeg)
-    LgLfnm1h = lie_derivative(sys.g, lie_derivative(sys.f, sys.h, reldeg - 1))
-    A, b, c = ref.A, ref.B, ref.C
+    if output is None:
+        assert sys.n_outputs == ref.n_outputs == 1, "systems must be single output"
+        h = sys.h
+        A, b, c = ref.A, ref.B, ref.C
+    else:
+        h = lambda x, t=None: sys.h(x, t=t)[output]
+        A, b, c = ref.A, ref.B, ref.C[output]
+
+    Lfnh = lie_derivative(sys.f, h, reldeg)
+    LgLfnm1h = lie_derivative(sys.g, lie_derivative(sys.f, h, reldeg - 1))
     cAn = c.dot(np.linalg.matrix_power(A, reldeg))
     cAnm1b = c.dot(np.linalg.matrix_power(A, reldeg - 1)).dot(b)
 
--- tests/test_linearize.py
+++ tests/test_linearize.py
@@ -15,29 +15,30 @@ from dynax.linearize import input_output_linearize, is_controllable, relative_de
 tols = dict(rtol=1e-04, atol=1e-06)
 
 
+class SpringMassDamperWithOutput(ControlAffine):
+    m = 0.1
+    r = 0.1
+    k = 0.1
+    out: int
+    n_states = 2
+    n_inputs = 1
+    n_outputs = 1
+
+    def __init__(self, out):
+        self.out = out
+
+    def f(self, x, t=None):
+        x1, x2 = x
+        return jnp.array([x2, (-self.r * x2 - self.k * x1) / self.m])
+
+    def g(self, x, t=None):
+        return jnp.array([0, 1 / self.m])
+
+    def h(self, x, t=None):
+        return x[np.array(self.out)]
+
+
 def test_relative_degree():
-    class SpringMassDamperWithOutput(ControlAffine):
-        m = 0.1
-        r = 0.1
-        k = 0.1
-        out: int
-        n_states = 2
-        n_inputs = 1
-        n_outputs = 1
-
-        def __init__(self, out):
-            self.out = out
-
-        def f(self, x, t=None):
-            x1, x2 = x
-            return jnp.array([x2, (-self.r * x2 - self.k * x1) / self.m])
-
-        def g(self, x, t=None):
-            return jnp.array([0, 1 / self.m])
-
-        def h(self, x, t=None):
-            return x[self.out]
-
     xs = np.random.normal(size=(100, 2))
     # output is position
     sys = SpringMassDamperWithOutput(out=0)
@@ -96,7 +97,7 @@ def test_linearize_sastry9_9():
     assert np.array_equal(linsys.D, [[0.0]])
 
 
-def test_input_output_linearize():
+def test_input_output_linearize_single_output():
     """Feedback linearized system equals system linearized around x0."""
     sys = NonlinearDrag(0.1, 0.1, 0.1, 0.1)
     ref = sys.linearize()
@@ -111,3 +112,20 @@ def test_input_output_linearize():
         Flow(feedback_sys)(np.zeros(feedback_sys.n_states), t, u)[1],
         **tols
     )
+
+
+def test_input_output_linearize_multiple_outputs():
+    """Can select an output for linearization."""
+    sys = SpringMassDamperWithOutput(out=[0, 1])
+    ref = sys.linearize()
+    for out_idx in range(2):
+        out_idx = 1
+        xs = np.random.normal(size=(100, sys.n_states))
+        reldeg = relative_degree(sys, xs, output=out_idx)
+        feedbacklaw = input_output_linearize(sys, reldeg, ref, output=out_idx)
+        feedback_sys = DynamicStateFeedbackSystem(sys, ref, feedbacklaw)
+        t = np.linspace(0, 1)
+        u = np.sin(t) * 0.1
+        y_ref = Flow(ref)(np.zeros(sys.n_states), t, u)[1]
+        y = Flow(feedback_sys)(np.zeros(feedback_sys.n_states), t, u)[1]
+        npt.assert_allclose(y_ref[:, out_idx], y[:, out_idx], **tols)
