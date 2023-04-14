e2ab42835e7ccf6faf72c4ac956ec4e008af968c
fhchl <fhchl@users.noreply.github.com>
2023-04-14 15:16:35 +0200

    export more

--- dynax/__init__.py
+++ dynax/__init__.py
@@ -1,9 +1,15 @@
 # TODO: leave out or make clear somewhere
 import jax as _jax
+from equinox import static_field
 
 from .derivative import lie_derivative
-from .estimation import fit_csd_matching, fit_least_squares, fit_multiple_shooting
-from .evolution import Flow, Map
+from .estimation import (
+    fit_csd_matching,
+    fit_least_squares,
+    fit_multiple_shooting,
+    non_negative_field,
+)
+from .evolution import AbstractEvolution, Flow, Map
 from .interpolation import spline_it
 from .system import (
     ControlAffine,
