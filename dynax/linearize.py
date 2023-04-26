ee32d7c2ce642caad89a0089ca61465918e1a308
fhchl <fhchl@users.noreply.github.com>
2023-04-26 13:40:18 +0200

    feat: allow asymptotic tracking

--- dynax/linearize.py
+++ dynax/linearize.py
@@ -1,7 +1,7 @@
 """Functions related to feedback linearization of nonlinear systems."""
 
 from collections.abc import Callable
-from typing import Optional
+from typing import Optional, Sequence
 
 import jax
 import numpy as np
@@ -44,22 +44,27 @@ def is_controllable(A, B) -> bool:
 
 
 def input_output_linearize(
-    sys: ControlAffine, reldeg: int, ref: LinearSystem, output: Optional[int] = None
+    sys: ControlAffine,
+    reldeg: int,
+    ref: LinearSystem,
+    output: Optional[int] = None,
+    asymptotic: Sequence | None = None,
 ) -> Callable[[Array, Array, float], float]:
     """Construct input-output linearizing feedback law.
 
     Args:
         sys: nonlinear model with single input
-        reldeg: relative degree of `sys` and `ref`
+        reldeg: relative degree of `sys` and lower bound of relative degree of `ref`
         ref: target model with single input
         output: specify linearizing output if systems have multiple outputs
+        asymptotic: If `None`, compute the exactly linearizing law. Otherwise,
+            a sequence of length `reldeg` defining the tracking behaviour.
 
     Note:
         Relative degree of `ref` must be same or higher than degree of `sys`.
         Only single-input-single-output systems are currently supported.
 
     """
-    # TODO: add options for reference `normal_form` or zeros of polynomials
     assert sys.n_inputs == ref.n_inputs == 1, "systems must be single input"
 
     if output is None:
@@ -75,7 +80,27 @@ def input_output_linearize(
     cAn = c.dot(np.linalg.matrix_power(A, reldeg))
     cAnm1b = c.dot(np.linalg.matrix_power(A, reldeg - 1)).dot(b)
 
-    def feedbacklaw(x: Array, z: Array, v: float) -> float:
-        return ((-Lfnh(x) + cAn.dot(z) + cAnm1b * v) / LgLfnm1h(x)).squeeze()
+    if asymptotic is None:
+
+        def feedbacklaw(x: Array, z: Array, v: float) -> float:
+            y_reldeg_ref = cAn.dot(z) + cAnm1b * v
+            y_reldeg = Lfnh(x)
+            return ((y_reldeg_ref - y_reldeg) / LgLfnm1h(x)).squeeze()
+
+    else:
+        msg = f"asymptotic must be of length {reldeg} but is {len(asymptotic)}"
+        assert len(asymptotic) == reldeg, msg
+        alphas = asymptotic
+
+        cAis = [c.dot(np.linalg.matrix_power(A, i)) for i in range(reldeg)]
+        Lfihs = [lie_derivative(sys.f, h, i) for i in range(reldeg)]
+
+        def feedbacklaw(x: Array, z: Array, v: float) -> float:
+            y_reldeg_ref = cAn.dot(z) + cAnm1b * v
+            y_reldeg = Lfnh(x)
+            ae0s = [
+                a * (Lfih(x) - cAi.dot(z)) for a, Lfih, cAi in zip(alphas, Lfihs, cAis)
+            ]
+            return ((y_reldeg_ref - y_reldeg - sum(ae0s)) / LgLfnm1h(x)).squeeze()
 
     return feedbacklaw
