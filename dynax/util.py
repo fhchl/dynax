4901add804efed1f99f20405d43eb620a00b81c5
fhchl <fhchl@users.noreply.github.com>
2024-07-21 23:04:37 +0200

    add type hints for utils.py

--- dynax/util.py
+++ dynax/util.py
@@ -1,46 +1,54 @@
 import functools
-from typing import Literal
+from typing import Callable, Literal
 
 import equinox
 import jax
 import jax.numpy as jnp
 
+from .custom_types import Array, Scalar
 
-def value_and_jacfwd(f, x):
+
+def value_and_jacfwd(fun: Callable, x: Array) -> tuple[Array, Callable]:
     """Create a function that evaluates both fun and its foward-mode jacobian.
 
-    Only works on ndarrays, not pytrees.
-    Source: https://github.com/google/jax/pull/762#issuecomment-1002267121
+    Args:
+        fun: Function whose Jacobian is to be computed.
+        x: Point at which function and Jacobian is evaluated.
+
+    From this `issue <https://github.com/google/jax/pull/762#issuecomment-1002267121>`_.
     """
-    pushfwd = functools.partial(jax.jvp, f, (x,))
+    pushfwd = functools.partial(jax.jvp, fun, (x,))
     basis = jnp.eye(x.size, dtype=x.dtype)
     y, jac = jax.vmap(pushfwd, out_axes=(None, 1))((basis,))
     return y, jac
 
 
-def value_and_jacrev(f, x):
+def value_and_jacrev(fun: Callable, x: Array) -> tuple[Array, Callable]:
     """Create a function that evaluates both fun and its reverse-mode jacobian.
 
-    Only works on ndarrays, not pytrees.
-    Source: https://github.com/google/jax/pull/762#issuecomment-1002267121
+    Args:
+        fun: Function whose Jacobian is to be computed.
+        x: Point at which function and Jacobian is evaluated.
+
+    From this `issue <https://github.com/google/jax/pull/762#issuecomment-1002267121>`_.
     """
-    y, pullback = jax.vjp(f, x)
+    y, pullback = jax.vjp(fun, x)
     basis = jnp.eye(y.size, dtype=y.dtype)
     jac = jax.vmap(pullback)(basis)
     return y, jac
 
 
-def mse(target, prediction, axis=0):
+def mse(target: Array, prediction: Array, axis: int = 0) -> Scalar:
     """Compute mean-squared error."""
     return jnp.mean(jnp.abs(target - prediction) ** 2, axis=axis)
 
 
-def nmse(target, prediction, axis=0):
+def nmse(target: Array, prediction: Array, axis: int = 0) -> Scalar:
     """Compute normalized mean-squared error."""
     return mse(target, prediction, axis) / jnp.mean(jnp.abs(target) ** 2, axis=axis)
 
 
-def nrmse(target, prediction, axis=0):
+def nrmse(target: Array, prediction: Array, axis: int = 0) -> Scalar:
     """Compute normalized root mean-squared error."""
     return jnp.sqrt(nmse(target, prediction, axis))
 
