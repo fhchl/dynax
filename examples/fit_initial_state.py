54c27251273601825521c9fb45db16f32b1d565c
fhchl <fhchl@users.noreply.github.com>
2024-09-22 22:34:22 +0200

    update fit_initial_state.py

--- examples/fit_initial_state.py
+++ examples/fit_initial_state.py
@@ -1,101 +1,55 @@
-"""Example: fit a second-order nonlinear system to data."""
+"""Fit a parameters and initial values of an ODE."""
 
-import jax.numpy as jnp
+import jax
 import matplotlib.pyplot as plt
 import numpy as np
 
-from dynax import (
-    AbstractControlAffine,
-    field,
-    fit_csd_matching,
-    fit_least_squares,
-    Flow,
+from dynax import field, fit_least_squares, Flow, pretty
+from dynax.example_models import NonlinearDrag
+
+
+# Declare the initial values as trainable using the `field` function.
+class NonlinearDragWithInitialValues(NonlinearDrag):
+    initial_state: jax.Array = field(init=True)
+
+
+# Initiate a dynamical system representing the some "true" parameters.
+true_system = NonlinearDragWithInitialValues(
+    m=1.0, r=2.0, r2=0.1, k=4.0, initial_state=(0.5, 1.0)
 )
 
-
-# Define a dynamical system of the form
-#
-# ẋ = f(x) + g(x)u
-# y = h(x)
-#
-# The `ControlAffine` class inherits from eqinox.Module which inherits from
-# `dataclasses.dataclass`.
-class NonlinearDrag(AbstractControlAffine):
-    """Spring-mass-damper system with nonliner drag.
-
-    .. math:: m ẍ +  r ẋ + r2 ẋ |ẋ| + k x = u
-              y = x
-
-    """
-
-    # Declare parameters as dataclass fields.
-    m: float = field()
-    r: float = field()
-    r2: float = field()
-    k: float = field()
-
-    # The initial_state attribute is static by default. If we want to make it learnable
-    # we must declare it using the `field` function.
-    initial_state: jnp.ndarray = field(init=True)
-
-    n_inputs = "scalar"
-
-    # Define the dynamical system via the methods f, g, and h
-    def f(self, x):
-        x1, x2 = x
-        return jnp.array(
-            [x2, (-self.r * x2 - self.r2 * jnp.abs(x2) * x2 - self.k * x1) / self.m]
-        )
-
-    def g(self, x):
-        return jnp.array([0.0, 1.0 / self.m])
-
-    def h(self, x):
-        return x[0]
-
-
-# initiate a dynamical system representing the some "true" parameters
-true_system = NonlinearDrag(
-    m=1.0, r=2.0, r2=0.1, k=4.0, initial_state=jnp.array([1.0, 1.0])
-)
-# combine ODE system with ODE solver (Dopri5 and constant stepsize by default)
+# Combine ODE system and ODE solver (Dopri5 and constant stepsize by default).
 true_model = Flow(true_system)
-print("true system:", true_system)
+print("true system:", pretty(true_system))
 
-# some training data using the true model. This could be your measurement data.
-t_train = np.linspace(0, 10, 1000)
+# Create some training data using the true model. This could be your measurement data.
+t_train = np.linspace(0, 2, 100)
 samplerate = 1 / t_train[1]
 np.random.seed(42)
 u_train = np.random.normal(size=len(t_train))
 x_train, y_train = true_model(t_train, u_train)
 
-# create our model system with some initial parameters
-initial_sys = NonlinearDrag(
-    m=1.0, r=1.0, r2=1.0, k=1.0, initial_state=jnp.array([0.0, 0.0])
+# Create our model system with some initial parameters.
+initial_sys = NonlinearDragWithInitialValues(
+    m=1.0, r=1.0, r2=1.0, k=1.0, initial_state=(0.0, 0.0)
 )
-print("initial system:", initial_sys)
+print("initial system:", pretty(initial_sys))
 
-# If we have long-duration, wide-band input data we can fit the linear
-# parameters by matching the transfer-functions. In this example the result is
-# not very good.
-initial_sys = fit_csd_matching(
-    initial_sys, u_train, y_train, samplerate, nperseg=100
-).result
-print("linear params fitted:", initial_sys)
-
-# Combine the ODE with an ODE solver
+# Combine the ODE with an ODE solver.
 init_model = Flow(initial_sys)
-# Fit all parameters with previously estimated parameters as a starting guess.
+
+# Fit the parameters of the nonlinear system including the initial state.
 pred_model = fit_least_squares(
     model=init_model, t=t_train, y=y_train, u=u_train, verbose=0
 ).result
-print("fitted system:", pred_model.system)
+print("fitted system:", pretty(pred_model.system))
 
-# check the results
+# Check the results.
 x_pred, y_pred = pred_model(t_train, u_train)
 assert np.allclose(x_train, x_pred)
+assert np.allclose(true_system.initial_state, pred_model.system.initial_state)
 
-plt.plot(t_train, x_train, label="target")
-plt.plot(t_train, x_pred, "--", label="prediction")
+plt.plot(t_train, x_train, "--", label="target")
+plt.plot(t_train, x_pred, label="prediction")
 plt.legend()
 plt.show()
