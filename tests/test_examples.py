a823a51c6d189ac4d7566f49c0438b61bbc8b459
fhchl <fhchl@users.noreply.github.com>
2024-10-19 16:03:10 +0200

    convert paths to strings to avoid bug in runpy
    See https://github.com/python/cpython/issues/99437

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,23 +1,24 @@
-import pathlib
 import runpy
+from pathlib import Path
 
 import nbformat
 import pytest
 from nbconvert.preprocessors import ExecutePreprocessor
 
 
-examples = pathlib.Path(__file__, "..", "..", "examples").resolve().glob("*.py")
-notebooks = pathlib.Path(__file__, "..", "..", "examples").resolve().glob("*.ipynb")
+example_dir = Path(__file__, "..", "..", "examples").resolve()
+examples = [str(p) for p in example_dir.glob("*.py")]
+notebooks = [str(p) for p in example_dir.resolve().glob("*.ipynb")]
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize("example", examples, ids=lambda x: str(x.name))
+@pytest.mark.parametrize("example", examples, ids=lambda x: Path(x).name)
 def test_examples_run_without_error(example):
     runpy.run_path(example)
 
 
 @pytest.mark.slow
-@pytest.mark.parametrize("notebook", notebooks, ids=lambda x: str(x.name))
+@pytest.mark.parametrize("notebook", notebooks, ids=lambda x: Path(x).name)
 def test_notebooks_dont_change(notebook):
     with open(notebook) as f:
         nb = nbformat.read(f, as_version=4)
